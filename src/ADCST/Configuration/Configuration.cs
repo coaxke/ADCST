6a52bb037ed6d4efda8f3c1df23e0bcd00856bf8
squip <patrick.squire@ptml.co.nz>
2015-03-30 22:38:37 +1300

    Initial Checkin

---
+++ src/ADCST/Configuration/Configuration.cs
@@ -0,0 +1,78 @@
+﻿using System.Configuration;
+
+namespace ADCST.Configuration
+{
+
+        public interface IConfiguration
+        {
+            string TenantName { get; }
+            string TenantID { get; }
+            string ClientId { get; }
+            string ClientSecret { get; }
+            string ClientIdForUserAuth { get; }
+            string AuthString { get; }
+            string ResourceURL { get; }
+        }
+
+        public class ADCSTConfiguration : IConfiguration
+        {
+            public string TennantName
+            {
+                get
+                {
+                    return ConfigurationManager.AppSettings["TenantName"];
+                }
+            }
+
+            public string TenantID
+            {
+                get
+                {
+                    return ConfigurationManager.AppSettings["TenantID"];
+                }
+            }
+
+            public string ClientId
+            {
+                get
+                {
+                    return ConfigurationManager.AppSettings["ClientId"];
+                }
+            }
+
+            public string ClientSecret
+            {
+                get
+                {
+                    return ConfigurationManager.AppSettings["ClientSecret"];
+                }
+            }
+
+            public string ClientIdForUserAuth
+            {
+                get
+                {
+                    return ConfigurationManager.AppSettings["ClientIdForUserAuth"];
+                }
+            }
+
+            public string AuthString
+            {
+                get
+                {
+                    string FullAuthString = string.Concat("https://login.windows.net/", TennantName);
+
+                    return FullAuthString;
+                }
+            }
+
+            public string ResourceURL
+            {
+                get
+                {
+                    return "https://graph.windows.net";
+                }
+           }        
+       }
+   }
+
---
+++ src/ADCST/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NLog;
+using ADCST.Configuration;
+
+namespace ADCST
+{
+   public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            IConfiguration config = new ADCSTConfiguration();
+            Logger Logger = LogManager.GetCurrentClassLogger();
+
+
+            string arg = args.Length == 0 ? string.Empty : args[0];
+
+            return Environment.ExitCode;
+
+        }
+    }
+}
